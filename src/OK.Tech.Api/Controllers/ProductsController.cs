a78ce4ca4b7512c949686d7b12742174ce0e7c2f
baracat <joaodobingo@gmail.com>
2021-04-15 14:09:13 -0300

    Add project files.

---
+++ src/OK.Tech.Api/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OK.Tech.Api.Models;
+using System.Collections.Generic;
+
+namespace OK.Tech.Api.Controllers
+{
+    [ApiController]
+    [Route("Products")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductViewModel>> GetProducts()
+        {
+
+            return Ok(new List<ProductViewModel>());
+        }
+    }
+}
---
+++ src/OK.Tech.Api/Models/ProductViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OK.Tech.Api.Models
+{
+    public class ProductViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+        
+        [Required(ErrorMessage = "The {0} must be supplied")]
+        [StringLength(200, ErrorMessage ="The {0} lenght must be between {2} and {1}", MinimumLength = 3)]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "The {0} must be supplied")]
+        [StringLength(1000, ErrorMessage = "The {0} lenght must be between {2} and {1}", MinimumLength = 3)]
+        public string Description { get; set; }
+        
+        [Required(ErrorMessage = "The {0} must be supplied")]
+        public decimal Price { get; set; }
+        //public DateTime CreateDate { get; set; }
+        
+        public bool Active { get; set; }
+
+    }
+}
