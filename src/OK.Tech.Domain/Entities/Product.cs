07697d00bdac17958b434671187a5218aa3ef7a3
baracat <joaodobingo@gmail.com>
2021-04-22 21:59:50 -0300

    Instalando EFCore e criando context

--- src/OK.Tech.Domain/Entities/Product.cs
+++ src/OK.Tech.Domain/Entities/Product.cs
@@ -5,10 +5,10 @@ namespace OK.Tech.Domain.Entities
     public class Product : Entity
     {
 
-        //public string Name { get; set; }
-        //public string Description { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
         //public decimal Price { get; set; }
         //public DateTime CreateDate { get; set; }
-        //public bool Active { get; set; }
+        public bool Active { get; set; }
     }
 }
