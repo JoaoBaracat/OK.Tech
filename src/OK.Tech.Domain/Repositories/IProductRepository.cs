bbfbff1c0f40824b3126e17109decc21af9cdbcc
baracat <joaodobingo@gmail.com>
2021-04-26 22:00:37 -0300

    feat: Add initial migration and Repositories

---
+++ src/OK.Tech.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using OK.Tech.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OK.Tech.Domain.Repositories
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+
+    }
+}
